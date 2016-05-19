b182d2da052998c4d546db6967e0b100636c8670
SBRKS <sbrks3@gmail.com>
2016-05-19 15:12:03 -0700

    edits style, more random function edits

--- script.js
+++ script.js
@@ -1,110 +1,138 @@
+	
+/*
+
+//one attempt at assigning 
+var randomNumber = Math.floor(Math.random()*questionSource.length);
+var selectedQuestion = questionSource[randomNumber].question;
+console.log(selectedQuestion);
+
+var selectedAnswer = function () {
+	var x = selectedQuestion.indexOf;
+	return questionSource.x.choice;
+
+};
+
+
+console.log(selectedAnswer);
+
+
+
+//another stab at assigning corresponding answer to random question
+if (newproperty-index?) = X, questionSource[X].answer;
+
+if qnum === 1 {
+	return questionSource[0].answer
+}
+
+*/
+
+
 	$(document).ready(function(){
 		var questionSource =[
 	{
 		question: "Which of the following is NOT an acceptable way to create an object?",
-		choices: ["var myObject = new Object();", "var myObject = {}", "*myObject function() = {}", "function Object (prop1, prop2) { this.prop1=a; this.prop2=b}"]
+		choices: ["var myObject = new Object();", "var myObject = {}", "*myObject function() = {}", "function Object (prop1, prop2) { this.prop1=a; this.prop2=b}"],
+		answer: "myObject function() = {}"
 	},
 
 	{
 		question: "What does 'this' mean?",
-		choices: ["*'This' is a keyword that refers to the current object.", "'This' what?", "The name of the function being called.", "'This' is a jQuery method."],
-		answer: [0]
+		choices: ["'This' is a keyword that refers to the current object.", "'This' what?", "The name of the function being called.", "'This' is a jQuery method."],
+		answer: "'This' is a keyword that refers to the current object."
 	},
 
 
 	{
 		question: "What does a for...in loop do?",
 		choices: ["It's a method that iterates over an array", "*It's a method that traverses all enumerable properties of an object and its prototype chain.", "For every index value in array, it calls a specified function", "For...in? More like foreign."],
-		answer: [1]
+		answer: "It's a method that traverses all enumerable properties of an object and its prototype chain."
 	},
 
 
 	{
 		question: "What does 'true || false' evaluate to?",
 		choices: ["True", "*False", "Null", "Undefined"],
-		answer: [1]
+		answer: "*False"
 	},
 
 
 	{
 		question: "How do you find the square root of a number?",
 		choices: ["Math.pow(num1, num2)", "Math.square(num)", "Math.root(num)", "*Math.sqrt(num)"],
-		answer: [3]
+		answer: "*Math.sqrt(num)"
 	},
 
 
 	{
 		question: "How do you create scope?",
 		choices: ["Declare a variable.", "Write a function.", "Use a callback function.", "Create an object."],
-		answer: [1]
+		answer: "Write a function."
 	},
 
 
 	{
 		question: "What is AJAX used for?",
 		choices: ["To request asynchronous events...", "As a front-end framework.", "To clean the kitchen.", "DOM manipulation."],
-		answer: [0]
+		answer: "'This' is a keyword that refers to the current object."
 	},
 
 
 	{
 		question: "How do you create an object?",
 		choices: ["Brackets, key/value pairs", "XX", "XX", "XX"],
-		answer: [3]
+		answer: "'This' is a keyword that refers to the current object."
 	},
 
 
 	{
 		question: "Why is the .Map function useful?",
 		choices: ["It makes calls to the Google Maps API", "It's a cleaner way to write a for loop", "It concatenates arrays.", "It iterates through an array and performs a specified action on each index value."],
-		answer: [3]
+		answer: "It iterates through an array and performs a specified action on each index value."
 	},
 
 
 	{
 		question: "What's the difference between a for and while loop?",
 		choices: ["For makes use of an iterator XX", "XX", "XX)", "XX)"],
-		answer: [3]
+		answer: "testanswer1"
 	},
 
 	{
 		question: "Why is the .Map function useful?",
 		choices: ["It makes calls to the Google Maps API", "It's a cleaner way to write a for loop", "It concatenates arrays.", "It iterates through an array and performs a specified action on each index value."],
-		answer: [3]
+		answer: "testanswer2"
 	},
 
 	{
 		question: "What is jQuery?",
 		choices: ["A back end library", "A library to make API calls", "A front-end framework.", "A CSS framework."],
-		answer: [3]
+		answer: "testanswer3"
 	},
 
 	{
 		question: "How do you write a function in jQuery?",
 		choices: ["It makes calls to the Google Maps API", "It's a cleaner way to write a for loop", "It concatenates arrays.", "It iterates through an array and performs a specified action on each index value."],
-		answer: [3]
+		answer: "testanswer4"
 	},
 
 	{
 		question: "Why would .forEach be preferable to a for loop?",
 		choices: ["It makes calls to the Google Maps API", "It's a cleaner way to write a for loop", "It concatenates arrays.", "It iterates through an array and performs a specified action on each index value."],
-		answer: [3]
+		answer: "testanswer5"
 	},
 
 	{
 		question: "Why is the .Map function useful?",
 		choices: ["It makes calls to the Google Maps API", "It's a cleaner way to write a for loop", "It concatenates arrays.", "It iterates through an array and performs a specified action on each index value."],
-		answer: [3]
+		answer: "testanswer6"
 	}
 
 ];
 
 	$('h2').addClass('animated bounceInDown');
 	$('a').addClass('animated bounceInUp');
+	$('button').addClass('animated bounceInUp'); //card flip not working
 		
-
-
-
 
 	$('.btn-success').click(function(event){
 			//define the containers of the info we target
@@ -119,7 +147,7 @@
 	for(i=0; i<=sourceLength; i++){
 	var newQuestionText = questionSource[randomNumber].question;
 	//var newQuestionChoices = questionSource[randomNumber].choices;
-      var timeAnimation = 400;
+      var timeAnimation = 80;
       var cardFront = $('#cardFront');
       //fade out animation with callback
       cardFront.fadeOut(timeAnimation, function(){
@@ -127,6 +155,7 @@
 				cardFront.append('<p>'+newQuestionText+'</p>'); 
         
         //fadein animation.
+       	cardFront.addClass('animated flipinY');
         cardFront.fadeIn(timeAnimation);
       });  
 			
@@ -135,21 +164,7 @@
 	
 	});//end questionButton function
 
-/*
-var randomNumber = Math.floor(Math.random()*questionSource.length);
-var selectedQuestion = questionSource[randomNumber].question;
-console.log(selectedQuestion);
 
-var selectedAnswer = function () {
-	var x = selectedQuestion.indexOf;
-	return questionSource.x.choice;
-
-};
-
-
-console.log(selectedAnswer);
-
-*/
 
 //answer button event
 	$('.btn-info').click(function(event){
@@ -165,15 +180,16 @@ console.log(selectedAnswer);
 			//set a new quote
 			for(i=0; i<=sourceLength; i++){
 			//var newQuestionText = questionSource[randomNumber].question;
-			var newQuestionChoices = questionSource[randomNumber].choices;
-      var timeAnimation = 500;
+			var newAnswer = questionSource[randomNumber].answer;
+      var timeAnimation = 100;
       var cardBack = $('#cardBack');
       //fade out animation with callback
       cardBack.fadeOut(timeAnimation, function(){
         cardBack.html('');
-				cardBack.append('<p>'+newQuestionChoices+'</p>'); 
+				cardBack.append('<p>'+newAnswer+'</p>'); 
         
         //fadein animation.
+        cardBack.addClass('animated flipInY');
         cardBack.fadeIn(timeAnimation);
       });  
 			
